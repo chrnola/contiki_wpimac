503b30a28e9c02ccca6c28e5cd5443d836e4d6db
Chris Pinola <trogdor3222@gmail.com>
2012-11-04 23:24:16 -0500

    Initial commit

---
+++ wpimac.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2012, Worcester Polytechnic Institute.
+ * All rights reserved.
+ *
+ * This file is part of the Contiki operating system.
+ */
+
+/**
+ * \file
+ *         A MAC protocol implementation.
+ * \author
+ *         Chris Pinola <cpinola@wpi.edu>
+ */
+
+#ifndef __WPIMAC_H__
+#define __WPIMAC_H__
+
+#include "net/mac/rdc.h"
+
+extern const struct rdc_driver wpimac_driver;
+
+#endif /* __WPIMAC_H__ */
