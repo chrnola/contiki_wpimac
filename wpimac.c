39f9a4e147c1a129c4f99cfb98b9dccc6ec4a055
Chris Pinola <trogdor3222@gmail.com>
2012-12-08 17:46:48 -0500

    Reverted back to my original queuing model, doing it all from the send_packet call was a nightmare due to the poor concurrency options in Contiki. Just need to tweak the timings and add contention slots now.

--- wpimac.c
+++ wpimac.c
@@ -39,158 +39,63 @@
 
 static volatile unsigned char radio_is_on = 0;
 static volatile unsigned char current_slot = 0;
+static volatile unsigned char waiting_to_send = 0;
 static volatile unsigned char crankshaft_is_running = 0;
-static volatile unsigned char needed_slot = 0;
+static volatile unsigned char needed_slot = BROADCAST_SLOT;
 
 static unsigned int REGULAR_SLOT = (RTIMER_SECOND / 1000) * (CRANKSHAFT_PERIOD / TOTAL_SLOTS);
 static void advanceSlot(struct rtimer *t, void *ptr, int status);
-// static void schedule_outgoing_packet(unsigned char, mac_callback_t, void *, void *, uint16_t);
-static int real_send();
-// static char check_buffers(unsigned char);
+static void schedule_outgoing_packet(unsigned char, mac_callback_t, void *, struct queuebuf *);
+static void real_send(mac_callback_t, void *, struct queuebuf *);
+static char check_buffers(unsigned char);
 
 static struct rtimer taskSlot;
 
-// typedef struct QueuedPacket{
-//   mac_callback_t sent;
-//   void *ptr;
-//   void *packetheader;
-//   uint16_t totlen;
-//   struct QueuedPacket *next;
-// } QueuedPacket;
+typedef struct QueuedPacket{
+  mac_callback_t sent;
+  void *ptr;
+  struct queuebuf *packet;
+  struct QueuedPacket *next;
+} QueuedPacket;
 
-// QueuedPacket *QPQueue[TOTAL_SLOTS];
+QueuedPacket *QPQueue[TOTAL_SLOTS];
 
-/*---------------------------------------------------------------------------*/
-// static char check_buffers(unsigned char for_slot) {
-//   if(QPQueue[for_slot] == NULL){
-//     return 0;
-//   } else{
-//     return 1;
-//   }
-// }
-/*---------------------------------------------------------------------------*/
 static void send_packet(mac_callback_t sent, void *ptr) {
-  // schedule for proper slot
-  // if broadcast, slot 0
-  // if uni, slot of dest->u8[7]
+  struct queuebuf *packet;
 
-  //packetbuf_addr(PACKETBUF_ADDR_RECEIVER)->u8
-  rimeaddr_t *dest = (rimeaddr_t*)packetbuf_addr(PACKETBUF_ADDR_RECEIVER);
-  if(rimeaddr_cmp(packetbuf_addr(PACKETBUF_ADDR_RECEIVER), &rimeaddr_null)){
-    // printf("WPI-MAC-send_packet(), node ID: %u, GOT BROADCAST?\n", node_id);
-    // schedule for slot 0
-    // void *newbuff = malloc(PACKETBUF_SIZE + PACKETBUF_HDR_SIZE);
-    // schedule_outgoing_packet(BROADCAST_SLOT, sent, ptr, newbuff, packetbuf_copyto(newbuff));
-    needed_slot = BROADCAST_SLOT;
+  packet = queuebuf_new_from_packetbuf();
+  if(packet == NULL) {
+      /* No buffer available */
+      printf("xmac: send failed, no queue buffer available (of %u)\n", QUEUEBUF_CONF_NUM);
+      mac_call_sent_callback(sent, ptr, MAC_TX_ERR, 1);
   } else {
-    // unsigned char dest_node = dest->u8[7];
-    // printf("WPI-MAC-send_packet(), node ID: %u - GOT UNICAST? to %d\n", node_id, dest->u8[7]);
-    // schedule for slot dest_node
-    // void *newbuff = malloc(PACKETBUF_SIZE + PACKETBUF_HDR_SIZE);
-    // schedule_outgoing_packet(dest_node, sent, ptr, newbuff, packetbuf_copyto(newbuff));
-    needed_slot = dest->u8[7];
-  }
-  
-  int ret = real_send();
+    // schedule for proper slot
+    // if broadcast, slot 0
+    // if uni, slot of dest->u8[7]
 
-  mac_call_sent_callback(sent, ptr, ret, 1);
-
-  // orig send
-  // if(NETSTACK_FRAMER.create() < 0) {
-  //   /* Failed to allocate space for headers */
-  //   printf("nullrdc: send failed, too large header\n");
-  //   ret = MAC_TX_ERR_FATAL;
-  // } else {
-  //   switch(NETSTACK_RADIO.send(packetbuf_hdrptr(), packetbuf_totlen())) {
-  //   case RADIO_TX_OK:
-  //     ret = MAC_TX_OK;
-  //     break;
-  //   case RADIO_TX_COLLISION:
-  //     ret = MAC_TX_COLLISION;
-  //     break;
-  //   case RADIO_TX_NOACK:
-  //     ret = MAC_TX_NOACK;
-  //     break;
-  //   default:
-  //     ret = MAC_TX_ERR;
-  //     break;
-  //   }
-}
-/*---------------------------------------------------------------------------*/
-// static void schedule_outgoing_packet(unsigned char slot, mac_callback_t sent, void *ptr, void *packetheader, uint16_t totlen){
-//   if(QPQueue[slot] == NULL){
-//     QPQueue[slot] = (QueuedPacket*) malloc(sizeof(QueuedPacket));
-//     if(QPQueue[slot] == NULL){
-//       printf("CRANKSHAFT RAN OUT OF MEMORY.\n");
-//     } else{
-//       QPQueue[slot]->sent = sent;
-//       QPQueue[slot]->ptr = ptr;
-//       QPQueue[slot]->packetheader = packetheader;
-//       QPQueue[slot]->totlen = totlen;
-//       QPQueue[slot]->next = NULL;
-//     }
-//   } else {
-//     QueuedPacket *curr = QPQueue[slot];
-//     while(curr->next != NULL){
-//       curr = curr->next;
-//     }
-//     curr->next = (QueuedPacket*) malloc(sizeof(QueuedPacket));
-//     curr = curr->next;
-//     if(curr == NULL){
-//       printf("CRANKSHAFT RAN OUT OF MEMORY.\n");
-//     } else{
-//       curr->sent = sent;
-//       curr->ptr = ptr;
-//       curr->packetheader = packetheader;
-//       curr->totlen = totlen;
-//       curr->next = NULL;
-//     }
-//   }
-// }
-/*---------------------------------------------------------------------------*/
-static int real_send(){
-  int ret;
-  // struct queuebuf *packet;
-
-  packetbuf_set_addr(PACKETBUF_ADDR_SENDER, &rimeaddr_node_addr);
-  if(NETSTACK_FRAMER.create() < 0) {
-    /* Failed to allocate space for headers */
-    printf("wpimac: send failed, too large header\n");
-    ret = MAC_TX_ERR_FATAL;
-  } else {
-    while(radio_is_on == 0 && current_slot != needed_slot);
-    switch(NETSTACK_RADIO.send(packetbuf_hdrptr(), packetbuf_totlen())) {
-    case RADIO_TX_OK:
-      ret = MAC_TX_OK;
-      break;
-    case RADIO_TX_COLLISION:
-      ret = MAC_TX_COLLISION;
-      break;
-    case RADIO_TX_NOACK:
-      ret = MAC_TX_NOACK;
-      break;
-    default:
-      ret = MAC_TX_ERR;
-      break;
+    rimeaddr_t *dest = (rimeaddr_t*)packetbuf_addr(PACKETBUF_ADDR_RECEIVER);
+    if(rimeaddr_cmp(packetbuf_addr(PACKETBUF_ADDR_RECEIVER), &rimeaddr_null)){
+      // printf("WPI-MAC-send_packet(), node ID: %u, GOT BROADCAST?\n", node_id);
+      // schedule for slot 0
+      // void *newbuff = malloc(PACKETBUF_SIZE + PACKETBUF_HDR_SIZE);
+      // schedule_outgoing_packet(BROADCAST_SLOT, sent, ptr, newbuff, packetbuf_copyto(newbuff));
+      needed_slot = BROADCAST_SLOT;
+    } else {
+      // unsigned char dest_node = dest->u8[7];
+      // printf("WPI-MAC-send_packet(), node ID: %u - GOT UNICAST? to %d\n", node_id, dest->u8[7]);
+      // schedule for slot dest_node
+      // void *newbuff = malloc(PACKETBUF_SIZE + PACKETBUF_HDR_SIZE);
+      // schedule_outgoing_packet(dest_node, sent, ptr, newbuff, packetbuf_copyto(newbuff));
+      needed_slot = dest->u8[7];
     }
 
+    schedule_outgoing_packet(needed_slot, sent, ptr, packet);
+
   }
-
-  // if(ret == MAC_TX_OK){
-  //   QueuedPacket *head = QPQueue[current_slot];
-  //   QPQueue[current_slot] = head->next;
-  //   head->ptr = NULL;
-  //   free(head->packetheader);
-  //   head->packetheader = NULL;
-  //   head->next = NULL;
-  //   free(head);
-  // }
-
-  return ret;
 }
 /*---------------------------------------------------------------------------*/
 static void send_list(mac_callback_t sent, void *ptr, struct rdc_buf_list *buf_list){
-  printf("WPI-MAC-send_list(), node ID: %u\n", node_id);
+  // printf("WPI-MAC-send_list(), node ID: %u\n", node_id);
   if(buf_list != NULL) {
     queuebuf_to_packetbuf(buf_list->buf);
     send_packet(sent, ptr);
@@ -198,7 +103,7 @@ static void send_list(mac_callback_t sent, void *ptr, struct rdc_buf_list *buf_l
 }
 /*---------------------------------------------------------------------------*/
 static void packet_input(void){
-  printf("WPI-MAC-packet_input(), node ID: %u\n", node_id);
+  // printf("WPI-MAC-packet_input(), node ID: %u\n", node_id);
   if(NETSTACK_FRAMER.parse() < 0) {
     printf("nullrdc: failed to parse %u\n", packetbuf_datalen());
   } else {
@@ -227,7 +132,9 @@ static unsigned short channel_check_interval(void){
 }
 /*---------------------------------------------------------------------------*/
 static void advanceSlot(struct rtimer *t, void *ptr, int status){
-  rtimer_set(t, RTIMER_TIME(t) + REGULAR_SLOT, REGULAR_SLOT, (void (*)(struct rtimer *, void *))advanceSlot, NULL);
+  if(!(rtimer_set(t, RTIMER_TIME(t) + REGULAR_SLOT, REGULAR_SLOT, (void (*)(struct rtimer *, void *))advanceSlot, NULL) == RTIMER_OK)){
+    printf("%s\n", "Could not schedule task!!!!!");
+  }
   if(current_slot == TOTAL_SLOTS + 1){
     current_slot = BROADCAST_SLOT;
   } else{
@@ -237,17 +144,17 @@ static void advanceSlot(struct rtimer *t, void *ptr, int status){
     current_slot = BROADCAST_SLOT;
   }
   //printf("Slot is now %u\n", current_slot);
-  // unsigned char somethingToSend = check_buffers(current_slot);
-  if(current_slot == BROADCAST_SLOT || current_slot == node_id || current_slot == needed_slot){
+  unsigned char somethingToSend = check_buffers(current_slot);
+  if(current_slot == BROADCAST_SLOT || current_slot == node_id || somethingToSend){
     // we need to be on
     if(!radio_is_on) on();
 
     // see if we gotta send
-    // if((current_slot != node_id) && somethingToSend){
+    if((current_slot != node_id) && somethingToSend){
     //   // grab the necessary info from our queue
-    //   QueuedPacket *curr = QPQueue[current_slot];
-    //   real_send(curr->sent, curr->ptr, curr->packetheader, curr->totlen);
-    // } // else we just need to be awake to listen
+      QueuedPacket *curr = QPQueue[current_slot];
+      real_send(curr->sent, curr->ptr, curr->packet);
+    } // else we just need to be awake to listen
 
   } else{
     // we can snooze
@@ -257,27 +164,26 @@ static void advanceSlot(struct rtimer *t, void *ptr, int status){
 }
 /*---------------------------------------------------------------------------*/
 static void init(void){
-  on();
+  // on();
   current_slot = TOTAL_SLOTS + 1;
   crankshaft_is_running = 1;
   //must schedule recurring wakeups for broadcast and uni-recv
   //also schedule slot advance
-  rtimer_set(&taskSlot, RTIMER_NOW() + REGULAR_SLOT, REGULAR_SLOT, (void (*)(struct rtimer *, void *))advanceSlot, NULL);
-  // int i = 0;
-  // for(; i < TOTAL_SLOTS; i++){
-  //   QPQueue[i] = NULL;
-  // }
+  int t;
+  if(node_id < 10) { 
+    t = REGULAR_SLOT + 300;
+  } else {
+    t = REGULAR_SLOT + 240;
+  }
+  if(!(rtimer_set(&taskSlot, RTIMER_NOW() + t, 1, (void (*)(struct rtimer *, void *))advanceSlot, NULL) == RTIMER_OK)){
+    printf("%s\n", "Could not schedule initial task!!!!!");
+  }
 
-  // if(node_id == 1){
-  //   sendSyncMessage();
-  // } else{
-  //   waiting_for_sync = 1;
-  //   // printf("Waiting until we get the sync packet.\n");
-  //   // while(waiting_for_sync){
-  //   //   checkForSyncPacket();
-  //   // }
-  //   // printf("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! DONE WAITING!!!!!!\n");
-  // }
+  int i = 0;
+  for(; i < TOTAL_SLOTS; i++){
+    QPQueue[i] = NULL;
+  }
+
 }
 /*---------------------------------------------------------------------------*/
 const struct rdc_driver wpimac_driver = {
@@ -290,4 +196,89 @@ const struct rdc_driver wpimac_driver = {
   off,
   channel_check_interval,
 };
+/*---------------------------------------------------------------------------*/
+static void real_send(mac_callback_t sent, void *ptr, struct queuebuf *pkt){
+    int ret;
+
+    queuebuf_to_packetbuf(pkt);
+
+    packetbuf_set_addr(PACKETBUF_ADDR_SENDER, &rimeaddr_node_addr);
+    if(NETSTACK_FRAMER.create() < 0) {
+      /* Failed to allocate space for headers */
+      printf("wpimac: send failed, too large header\n");
+      ret = MAC_TX_ERR_FATAL;
+    } else {
+      // printf("%s - %u - %u\n", "Waiting to send...", current_slot, needed_slot);
+      // while(radio_is_on == 0 && !(current_slot == needed_slot)); //wish i knew why this didn't work
+      // rtimer_clock_t recent = RTIMER_TIME(&taskSlot);
+      // rtimer_clock_t until = calcNext(current_slot, needed_slot);
+      // while(RTIMER_CLOCK_LT(RTIMER_NOW(), recent + until));
+      // printf("%s - %u - %u\n", "sending!", current_slot, needed_slot);
+      switch(NETSTACK_RADIO.send(packetbuf_hdrptr(), packetbuf_totlen())) {
+      case RADIO_TX_OK:
+        ret = MAC_TX_OK;
+        break;
+      case RADIO_TX_COLLISION:
+        ret = MAC_TX_COLLISION;
+        break;
+      case RADIO_TX_NOACK:
+        ret = MAC_TX_NOACK;
+        break;
+      default:
+        ret = MAC_TX_ERR;
+        break;
+      }
+
+    }
+
+    mac_call_sent_callback(sent, ptr, ret, 1);
+
+    // remove packet from our queue
+    if(ret == MAC_TX_OK){
+      QueuedPacket *head = QPQueue[current_slot];
+      QPQueue[current_slot] = head->next;
+      head->ptr = NULL;
+      free(head->packet);
+      head->packet = NULL;
+      head->next = NULL;
+      free(head);
+    }
+}
+/*---------------------------------------------------------------------------*/
+static void schedule_outgoing_packet(unsigned char slot, mac_callback_t sent, void *ptr, struct queuebuf *pkt){
+  if(QPQueue[slot] == NULL){
+    QPQueue[slot] = (QueuedPacket*) malloc(sizeof(QueuedPacket));
+    if(QPQueue[slot] == NULL){
+      printf("CRANKSHAFT RAN OUT OF MEMORY.\n");
+    } else{
+      QPQueue[slot]->sent = sent;
+      QPQueue[slot]->ptr = ptr;
+      QPQueue[slot]->packet = pkt;
+      QPQueue[slot]->next = NULL;
+    }
+  } else {
+    QueuedPacket *curr = QPQueue[slot];
+    while(curr->next != NULL){
+      curr = curr->next;
+    }
+    curr->next = (QueuedPacket*) malloc(sizeof(QueuedPacket));
+    curr = curr->next;
+    if(curr == NULL){
+      printf("CRANKSHAFT RAN OUT OF MEMORY.\n");
+    } else{
+      curr->sent = sent;
+      curr->ptr = ptr;
+      curr->packet = pkt;
+      curr->next = NULL;
+    }
+  }
+}
+/*---------------------------------------------------------------------------*/
+static char check_buffers(unsigned char for_slot) {
+  if(QPQueue[for_slot] == NULL){
+    return 0;
+  } else{
+    return 1;
+  }
+}
 /*---------------------------------------------------------------------------*/
